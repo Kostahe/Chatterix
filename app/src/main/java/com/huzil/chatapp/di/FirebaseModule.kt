6f90e42da4a46f1394e7daaa1c57677312a5b6f8
Kostahe <huzilkon@gmail.com>
2024-05-28 00:37:39 +0200

    Fixed typo

--- app/src/main/java/com/huzil/chatapp/di/FirebaseModule.kt
+++ app/src/main/java/com/huzil/chatapp/di/FirebaseModule.kt
@@ -4,10 +4,11 @@ import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 object FirebaseModule {
 
     @Singleton
