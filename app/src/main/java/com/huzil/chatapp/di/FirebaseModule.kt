2ed88da68099ef5de2b105830adc3e78b6ae08b3
Kostahe <huzilkon@gmail.com>
2024-05-27 23:17:41 +0200

    Created FirebaseModule

---
+++ app/src/main/java/com/huzil/chatapp/di/FirebaseModule.kt
@@ -0,0 +1,14 @@
+package com.huzil.chatapp.di
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+object FirebaseModule {
+
+
+}
