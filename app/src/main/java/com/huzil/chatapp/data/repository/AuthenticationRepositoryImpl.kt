16cf60973bc4fe1a3629ff3316238fdc22e9c27c
Kostahe <huzilkon@gmail.com>
2024-05-27 23:54:58 +0200

    Implemented function register

--- app/src/main/java/com/huzil/chatapp/data/repository/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/huzil/chatapp/data/repository/AuthenticationRepositoryImpl.kt
@@ -2,10 +2,18 @@ package com.huzil.chatapp.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.huzil.chatapp.domain.repository.AuthenticationRepository
+import kotlin.coroutines.cancellation.CancellationException
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class AuthenticationRepositoryImpl @Inject constructor(
-
+    private val authentication: FirebaseAuth
 ) : AuthenticationRepository {
-
+    override suspend fun register(email: String, password: String) {
+        try {
+            authentication.createUserWithEmailAndPassword(email, password).await()
+        } catch (e: Exception) {
+            if (e is CancellationException) throw e
+        }
+    }
 }
