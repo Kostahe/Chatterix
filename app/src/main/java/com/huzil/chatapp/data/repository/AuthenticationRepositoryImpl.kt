f1d84757b37a5e767137a39da68c5500145c96b4
Kostahe <huzilkon@gmail.com>
2024-05-27 23:37:31 +0200

    Created class AuthenticationRepositoryImpl which implements interface AuthenticationRepository

---
+++ app/src/main/java/com/huzil/chatapp/data/repository/AuthenticationRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.huzil.chatapp.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+import com.huzil.chatapp.domain.repository.AuthenticationRepository
+import javax.inject.Inject
+
+class AuthenticationRepositoryImpl @Inject constructor(
+
+) : AuthenticationRepository {
+
+}
