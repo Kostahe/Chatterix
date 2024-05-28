070ab0a76e65a8e3cb1566dd1ec02c64e6b2e956
Konstantyn <gagarin2789@gmail.com>
2024-05-28 20:30:57 +0200

    Created AppNavGraph

---
+++ app/src/main/java/com/huzil/chatterix/presentation/AppNavGraph.kt
@@ -0,0 +1,41 @@
+package com.huzil.chatterix.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.huzil.chatterix.presentation.authentication.AuthenticationScreen
+import kotlinx.serialization.Serializable
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavGraphRoutes.AuthenticationNavigation
+    ) {
+        navigation<NavGraphRoutes.AuthenticationNavigation>(
+            startDestination = NavigationRoutes.AuthenticationScreen
+        ) {
+            composable<NavigationRoutes.AuthenticationScreen> {
+                AuthenticationScreen(
+                    onEmail = {},
+                    onGoogle = {}
+                )
+            }
+        }
+    }
+}
+
+sealed class NavGraphRoutes() {
+    @Serializable
+    object AuthenticationNavigation: NavGraphRoutes()
+}
+
+sealed class NavigationRoutes() {
+    @Serializable
+    object AuthenticationScreen: NavigationRoutes()
+}
