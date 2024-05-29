f44eabfe87946eaf66dfffd4e43e4b222a23b6db
Kostahe <huzilkon@gmail.com>
2024-05-29 18:04:08 +0200

    Added signInWithGoogle function

--- app/src/main/java/com/huzil/chatterix/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/huzil/chatterix/domain/repository/AuthenticationRepository.kt
@@ -2,4 +2,5 @@ package com.huzil.chatterix.domain.repository
 
 interface AuthenticationRepository {
     suspend fun register(email: String, password: String)
+    suspend fun signInWithGoogle()
 }
