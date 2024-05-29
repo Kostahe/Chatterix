e2a324f6281a676d16ad88cb0f3d9a13711d69b5
Konstantyn <gagarin2789@gmail.com>
2024-05-29 20:10:21 +0200

    Created AuthenticationCoordinator

---
+++ app/src/main/java/com/huzil/chatterix/presentation/authentication/AuthenticationCoordinator.kt
@@ -0,0 +1,28 @@
+package com.huzil.chatterix.presentation.authentication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+
+class AuthenticationCoordinator(
+    private val viewModel: AuthenticationViewModel,
+) {
+    val screenStateFlow = viewModel.stateFlow
+
+    fun onGoogleSignInClick() {
+
+    }
+
+    fun onEmailSignInClick() {
+
+    }
+ }
+
+@Composable
+fun rememberAuthenticationCoordinator(
+    viewModel: AuthenticationViewModel = hiltViewModel(),
+): AuthenticationCoordinator {
+    return remember(viewModel) {
+        AuthenticationCoordinator(viewModel)
+    }
+}
